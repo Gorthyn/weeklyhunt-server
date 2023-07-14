3e6f22e93c529aa23fef83c8ab232610f900abdc
Dylan Kline <dylanekline@gmail.com>
2023-07-14 13:20:01 -0500

    Basic Moves view added

---
+++ weeklyhuntapi/views/basic_moves.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from weeklyhuntapi.models import BasicMove
+
+class BasicMoveView(ViewSet):
+    """Weekly Hunt basic moves view"""
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single move"""
+        try:
+            move = BasicMove.objects.get(pk=pk)
+            serializer = BasicMoveSerializer(move)
+            return Response(serializer.data)
+        except BasicMove.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        """Handle GET requests to get all moves"""
+        moves = BasicMove.objects.all()
+        serializer = BasicMoveSerializer(moves, many=True)
+        return Response(serializer.data)
+
+class BasicMoveSerializer(serializers.ModelSerializer):
+    """JSON serializer for basic moves"""
+    class Meta:
+        model = BasicMove
+        fields = ('id', 'name')
