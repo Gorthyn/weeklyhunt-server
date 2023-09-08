a20c9b892db491af288d4fdad19855712d7ccdd1
Dylan Kline <dylanekline@gmail.com>
2023-09-07 21:14:43 -0500

    Created list/retrieve views for advanced improvements

---
+++ weeklyhuntapi/views/advanced_improvements.py
@@ -0,0 +1,29 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status, permissions
+from weeklyhuntapi.models import AdvancedImprovements
+
+class AdvancedImprovementsView(ViewSet):
+    """Weekly Hunt Advanced Improvements view"""
+
+    permission_classes = [permissions.AllowAny]
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single improvement"""
+        try:
+            improvement = AdvancedImprovements.objects.get(pk=pk)
+            serializer = AdvancedImprovementsSerializer(improvement)
+            return Response(serializer.data)
+        except AdvancedImprovements.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        """Handle GET requests to get all improvements"""
+        improvements = AdvancedImprovements.objects.all()
+        serializer = AdvancedImprovementsSerializer(improvements, many=True)
+        return Response(serializer.data)
+
+class AdvancedImprovementsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdvancedImprovements
+        fields = ('id', 'playbook', 'description')
