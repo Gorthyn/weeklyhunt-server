f5072167e640a785aa8916a019cba00bd5d300c0
Dylan Kline <dylanekline@gmail.com>
2023-09-15 14:54:56 -0500

    Completed list and retrieve view for Combat Effects

--- weeklyhuntapi/views/combat_effects.py
+++ weeklyhuntapi/views/combat_effects.py
@@ -2,3 +2,24 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status, permissions
 from weeklyhuntapi.models import CombatEffect
+
+class CombatEffectView(ViewSet):
+    permission_classes = [permissions.AllowAny]
+
+    def retrieve(self, request, pk=None):
+        try:
+            combat_effect = CombatEffect.objects.get(pk=pk)
+            serializer = CombatEffectSerializer(combat_effect)
+            return Response(serializer.data)
+        except CombatEffect.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        combat_effects = CombatEffect.objects.all()
+        serializer = CombatEffectSerializer(combat_effects, many=True)
+        return Response(serializer.data)
+    
+class CombatEffectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CombatEffect
+        fields = ('id', 'name', 'harm_bonus', 'type_bonus', 'attributes', 'special')
