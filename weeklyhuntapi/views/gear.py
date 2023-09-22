cbb9b27acc70a9aae55e4978fdc4b03468af5398
Dylan Kline <dylanekline@gmail.com>
2023-09-22 15:15:25 -0500

    Completed list and retrieve view for Gear

---
+++ weeklyhuntapi/views/gear.py
@@ -0,0 +1,25 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status, permissions
+from weeklyhuntapi.models import Gear
+
+class GearView(ViewSet):
+    permission_classes = [permissions.AllowAny]
+
+    def retrieve(self, request, pk=None):
+        try:
+            gear = Gear.objects.get(pk=pk)
+            serializer = GearSerializer(gear)
+            return Response(serializer.data)
+        except Gear.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        gears = Gear.objects.all()
+        serializer = GearSerializer(gears, many=True)
+        return Response(serializer.data)
+    
+class GearSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gear
+        fields = ('id', 'name', 'harm', 'playbook')
