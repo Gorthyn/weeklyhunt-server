d167331818cc183eb7c7fcf4fa5430a5199ae11a
Dylan Kline <dylanekline@gmail.com>
2023-09-22 15:11:49 -0500

    Completed list and retrieve view for Fate

---
+++ weeklyhuntapi/views/fate.py
@@ -0,0 +1,25 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status, permissions
+from weeklyhuntapi.models import Fate
+
+class FateView(ViewSet):
+    permission_classes = [permissions.AllowAny]
+
+    def retrieve(self, request, pk=None):
+        try:
+            fate = Fate.objects.get(pk=pk)
+            serializer = FateSerializer(fate)
+            return Response(serializer.data)
+        except Fate.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        fates = Fate.objects.all()
+        serializer = FateSerializer(fates, many=True)
+        return Response(serializer.data)
+    
+class FateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fate
+        fields = ('id', 'description')
