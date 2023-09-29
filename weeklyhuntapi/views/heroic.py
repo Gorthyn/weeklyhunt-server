be198c3a29b6bde52208c06af6c50591744df957
Dylan Kline <dylanekline@gmail.com>
2023-09-29 15:50:39 -0500

    Completed List and Retrieve view for Heroic

---
+++ weeklyhuntapi/views/heroic.py
@@ -0,0 +1,25 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status, permissions
+from weeklyhuntapi.models import Heroic
+
+class HeroicView(ViewSet):
+    permission_classes = [permissions.AllowAny]
+
+    def retrieve(self, request, pk=None):
+        try:
+            heroic = Heroic.objects.get(pk=pk)
+            serializer = HeroicSerializer(heroic)
+            return Response(serializer.data)
+        except Heroic.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        heroics = Heroic.objects.all()
+        serializer = HeroicSerializer(heroics, many=True)
+        return Response(serializer.data)
+    
+class HeroicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Heroic
+        fields = ('id', 'description')
