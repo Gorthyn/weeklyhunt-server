f457d0cce59e53cb5ffed1bfe5c5960278f4f00f
Dylan Kline <dylanekline@gmail.com>
2023-10-26 23:22:27 -0500

    1-8 urls

--- weeklyhuntapi/views/__init__.py
+++ weeklyhuntapi/views/__init__.py
@@ -1,2 +1,8 @@
 from .auth import login_user, register_user
 from .basic_moves import BasicMoveView
+from .advanced_improvements import AdvancedImprovementsView
+from .agency_goals import AgencyGoalsView
+from .agency import AgencyView
+from .background import BackgroundView
+from .character_gear import CharacterGearView
+from .character_histories import CharacterHistoryView
