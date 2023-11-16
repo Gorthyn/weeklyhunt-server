0cac7019e4ff73de925c1a7108dafb09def1af55
Dylan Kline <dylanekline@gmail.com>
2023-11-16 14:03:22 -0600

    Create, Update, and Destroy added to Chosen Materials.

--- weeklyhuntapi/views/chosen_materials.py
+++ weeklyhuntapi/views/chosen_materials.py
@@ -19,6 +19,26 @@ class ChosenMaterialView(ViewSet):
         serializer = ChosenMaterialSerializer(chosen_materials, many=True)
         return Response(serializer.data)
     
+    def create(self, request):
+        serializer = ChosenMaterialSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        material = ChosenMaterial.objects.get(pk=pk)
+        serializer = ChosenMaterialSerializer(material, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        material = ChosenMaterial.objects.get(pk=pk)
+        material.delete()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
 class ChosenMaterialSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChosenMaterial
