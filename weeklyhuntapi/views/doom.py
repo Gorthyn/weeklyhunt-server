69f456f6885fd6edc2aa79a2e7163657a98efc86
Dylan Kline <dylanekline@gmail.com>
2023-11-24 14:24:43 -0600

    Create, Update, and Delete added to Doom.

--- weeklyhuntapi/views/doom.py
+++ weeklyhuntapi/views/doom.py
@@ -19,6 +19,26 @@ class DoomView(ViewSet):
         serializer = DoomSerializer(dooms, many=True)
         return Response(serializer.data)
     
+    def create(self, request):
+        serializer = DoomSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        doom = Doom.objects.get(pk=pk)
+        serializer = DoomSerializer(doom, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        doom = Doom.objects.get(pk=pk)
+        doom.delete()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
 class DoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doom
