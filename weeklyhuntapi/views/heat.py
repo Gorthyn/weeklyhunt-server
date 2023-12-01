d114ed7989c08f0b13a2529dd8550eb34b50d75d
Dylan Kline <dylanekline@gmail.com>
2023-12-01 12:16:59 -0600

    Create, Update, and Destroy added to Heat.

--- weeklyhuntapi/views/heat.py
+++ weeklyhuntapi/views/heat.py
@@ -19,6 +19,26 @@ class HeatView(ViewSet):
         serializer = HeatSerializer(heats, many=True)
         return Response(serializer.data)
     
+    def create(self, request):
+        serializer = HeatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        heat = Heat.objects.get(pk=pk)
+        serializer = HeatSerializer(heat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        heat = Heat.objects.get(pk=pk)
+        heat.delete()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
 class HeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Heat
