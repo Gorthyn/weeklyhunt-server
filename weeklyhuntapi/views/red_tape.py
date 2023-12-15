440a2993220abb4762ef1c9a71a58e8a95c2a144
Dylan Kline <dylanekline@gmail.com>
2023-12-15 13:03:23 -0600

    Create, Update, and Destroy added to Red Tape.

--- weeklyhuntapi/views/red_tape.py
+++ weeklyhuntapi/views/red_tape.py
@@ -19,6 +19,26 @@ class RedTapeView(ViewSet):
         serializer = RedTapeSerializer(red_tapes, many=True)
         return Response(serializer.data)
     
+    def create(self, request):
+        serializer = RedTapeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        red_tape = RedTape.objects.get(pk=pk)
+        serializer = RedTapeSerializer(red_tape, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        red_tape = RedTape.objects.get(pk=pk)
+        red_tape.delete()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
 class RedTapeSerializer(serializers.ModelSerializer):
     class Meta:
         model = RedTape
