c4d18836d11a277894f28117ad5b87aef48d1017
Dylan Kline <dylanekline@gmail.com>
2023-12-23 15:55:12 -0600

    Initiated tests.py module for validation of pull requests.

--- weeklyhuntapi/tests.py
+++ weeklyhuntapi/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here. Will need to create tests to validate pull requests for future use in moving to the front end.
