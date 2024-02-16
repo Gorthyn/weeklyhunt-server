f55024c916e78b959e59e996ade31296d3cd23f8
Dylan Kline <dylanekline@gmail.com>
2024-02-16 17:16:09 -0600

    Setup of tests.py

--- weeklyhuntapi/tests.py
+++ weeklyhuntapi/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Character, Playbook
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework.test import APIClient
 
 # Create your tests here. Will need to create tests to validate pull requests for future use in moving to the front end.
