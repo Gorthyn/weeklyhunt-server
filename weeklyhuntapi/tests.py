b161c086a49fb5fa6f8cd07560c72ac362dc335b
Dylan Kline <dylanekline@gmail.com>
2024-02-16 17:17:08 -0600

    First test create.

--- weeklyhuntapi/tests.py
+++ weeklyhuntapi/tests.py
@@ -6,3 +6,10 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 
 # Create your tests here. Will need to create tests to validate pull requests for future use in moving to the front end.
+class CharacterModelTest(TestCase):
+    def test_character_creation(self):
+        playbook = Playbook.objects.create(name="The Divine", description="Description of The Divine.")
+        character = Character.objects.create(name="John Doe", playbook=playbook, charm=2, cool=1, sharp=1, tough=-1, weird=2)
+        self.assertEqual(character.name, "John Doe")
+        self.assertEqual(character.playbook.name, "The Divine")
+        self.assertEqual(character.charm, 2)
