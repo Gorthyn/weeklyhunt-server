7e0429afbad80949aa9b5da1d7135f579595235b
Dylan Kline <dylanekline@gmail.com>
2024-03-09 10:07:25 -0600

    Edit to basic_moves view.

--- weeklyhuntapi/views/basic_moves.py
+++ weeklyhuntapi/views/basic_moves.py
@@ -23,26 +23,6 @@ class BasicMoveView(ViewSet):
         serializer = BasicMoveSerializer(moves, many=True)
         return Response(serializer.data)
 
-    def create(self, request):
-        serializer = BasicMoveSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        move = BasicMove.objects.get(pk=pk)
-        serializer = BasicMoveSerializer(move, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({}, status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        move = BasicMove.objects.get(pk=pk)
-        move.delete()
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
-
 class BasicMoveSerializer(serializers.ModelSerializer):
     """JSON serializer for basic moves"""
     class Meta:
