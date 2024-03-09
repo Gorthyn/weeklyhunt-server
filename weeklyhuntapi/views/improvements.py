f03e02270e793ab386b6556574ae83e67625e5ba
Dylan Kline <dylanekline@gmail.com>
2024-03-09 10:13:53 -0600

    Edit to improvements view.

--- weeklyhuntapi/views/improvements.py
+++ weeklyhuntapi/views/improvements.py
@@ -18,26 +18,6 @@ class ImprovementView(ViewSet):
         improvements = Improvement.objects.all()
         serializer = ImprovementSerializer(improvements, many=True)
         return Response(serializer.data)
-    
-    def create(self, request):
-        serializer = ImprovementSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        improvement = Improvement.objects.get(pk=pk)
-        serializer = ImprovementSerializer(improvement, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({}, status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        improvement = Improvement.objects.get(pk=pk)
-        improvement.delete()
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
 class ImprovementSerializer(serializers.ModelSerializer):
     class Meta:
