5eb87dde4d1eab5a012a5992cc861c118a4d1dde
Dylan Kline <dylanekline@gmail.com>
2024-03-09 10:10:43 -0600

    Edit to ratings view.

--- weeklyhuntapi/views/ratings.py
+++ weeklyhuntapi/views/ratings.py
@@ -18,26 +18,6 @@ class RatingView(ViewSet):
         ratings = Rating.objects.all()
         serializer = RatingSerializer(ratings, many=True)
         return Response(serializer.data)
-    
-    def create(self, request):
-        serializer = RatingSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        rating = Rating.objects.get(pk=pk)
-        serializer = RatingSerializer(rating, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({}, status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        rating = Rating.objects.get(pk=pk)
-        rating.delete()
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
