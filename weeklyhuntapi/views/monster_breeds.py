06e631c0964d381718f02ea664e874da17095d83
Dylan Kline <dylanekline@gmail.com>
2024-03-13 17:47:37 -0500

    Changes to Monster Breed, Natural Attack, Reason, Red Tape.

--- weeklyhuntapi/views/monster_breeds.py
+++ weeklyhuntapi/views/monster_breeds.py
@@ -18,26 +18,6 @@ class MonsterBreedsView(ViewSet):
         monster_breeds = MonsterBreeds.objects.all()
         serializer = MonsterBreedsSerializer(monster_breeds, many=True)
         return Response(serializer.data)
-    
-    def create(self, request):
-        serializer = MonsterBreedsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        monster_breed = MonsterBreeds.objects.get(pk=pk)
-        serializer = MonsterBreedsSerializer(monster_breed, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({}, status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        monster_breed = MonsterBreeds.objects.get(pk=pk)
-        monster_breed.delete()
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
 class MonsterBreedsSerializer(serializers.ModelSerializer):
     class Meta:
--- weeklyhuntapi/views/reason.py
+++ weeklyhuntapi/views/reason.py
@@ -18,26 +18,6 @@ class ReasonView(ViewSet):
         reasons = Reason.objects.all()
         serializer = ReasonSerializer(reasons, many=True)
         return Response(serializer.data)
-    
-    def create(self, request):
-        serializer = ReasonSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        reason = Reason.objects.get(pk=pk)
-        serializer = ReasonSerializer(reason, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({}, status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        reason = Reason.objects.get(pk=pk)
-        reason.delete()
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
 class ReasonSerializer(serializers.ModelSerializer):
     class Meta:
