727ecdfcc8cfad496d9ef9e6719aa0ca541b2e01
Dylan Kline <dylanekline@gmail.com>
2024-04-26 15:59:09 -0500

    Added create, update, and delete to agency_goals.py

--- weeklyhuntapi/views/advanced_improvements.py
+++ weeklyhuntapi/views/advanced_improvements.py
@@ -22,6 +22,23 @@ class AdvancedImprovementsView(ViewSet):
         serializer = AdvancedImprovementsSerializer(improvements, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = AdvancedImprovementsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        improvement = AdvancedImprovements.objects.get(pk=pk)
+        serializer = AdvancedImprovementsSerializer(improvement, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        improvement = AdvancedImprovements.objects.get(pk=pk)
+        improvement.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class AdvancedImprovementsSerializer(serializers.ModelSerializer):
     class Meta:
