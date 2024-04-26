f315e3cb7cf467b328e27d597a891b66d2a8e922
Dylan Kline <dylanekline@gmail.com>
2024-04-26 16:02:13 -0500

    Added create, update, and delete to background.py

--- weeklyhuntapi/views/background.py
+++ weeklyhuntapi/views/background.py
@@ -21,6 +21,24 @@ class BackgroundView(ViewSet):
         serializer = BackgroundSerializer(backgrounds, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = BackgroundSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        background = Background.objects.get(pk=pk)
+        serializer = BackgroundSerializer(background, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        background = Background.objects.get(pk=pk)
+        background.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class BackgroundSerializer(serializers.ModelSerializer):
     """JSON serializer for background"""
     class Meta:
