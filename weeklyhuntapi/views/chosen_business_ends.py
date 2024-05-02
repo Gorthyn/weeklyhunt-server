df8a227edda7be3b9ef30607524d29cc077f2683
Dylan Kline <dylanekline@gmail.com>
2024-05-02 16:20:06 -0500

    Create, Update, Delete added to chesen_business_ends.py

--- weeklyhuntapi/views/chosen_business_ends.py
+++ weeklyhuntapi/views/chosen_business_ends.py
@@ -18,6 +18,24 @@ class ChosenBusinessEndView(ViewSet):
         chosen_business_ends = ChosenBusinessEnd.objects.all()
         serializer = ChosenBusinessEndSerializer(chosen_business_ends, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = ChosenBusinessEndSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        chosen_business_end = ChosenBusinessEnd.objects.get(pk=pk)
+        serializer = ChosenBusinessEndSerializer(chosen_business_end, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        chosen_business_end = ChosenBusinessEnd.objects.get(pk=pk)
+        chosen_business_end.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class ChosenBusinessEndSerializer(serializers.ModelSerializer):
     class Meta:
