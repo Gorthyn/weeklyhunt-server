2034bafbd218c37ef27099e1990b71237f9a04f7
Dylan Kline <dylanekline@gmail.com>
2024-05-11 16:24:32 -0500

    Create, Update, Delete added to curses.py

--- weeklyhuntapi/views/curses.py
+++ weeklyhuntapi/views/curses.py
@@ -19,6 +19,24 @@ class CurseView(ViewSet):
         serializer = CurseSerializer(curses, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = CurseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        curse = Curse.objects.get(pk=pk)
+        serializer = CurseSerializer(curse, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        curse = Curse.objects.get(pk=pk)
+        curse.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class CurseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curse
