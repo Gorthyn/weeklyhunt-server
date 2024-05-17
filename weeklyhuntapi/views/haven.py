e77b2651b5b3332d984ea00fa48c4590cf7d7b78
Dylan Kline <dylanekline@gmail.com>
2024-05-17 15:32:19 -0500

    Create, Update, Delete added to haven.py

--- weeklyhuntapi/views/haven.py
+++ weeklyhuntapi/views/haven.py
@@ -19,6 +19,24 @@ class HavenView(ViewSet):
         serializer = HavenSerializer(havens, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = HavenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        haven = Haven.objects.get(pk=pk)
+        serializer = HavenSerializer(haven, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        haven = Haven.objects.get(pk=pk)
+        haven.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class HavenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Haven
