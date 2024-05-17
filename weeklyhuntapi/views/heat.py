072f63bb160880ac53aa600985594cef53a5a600
Dylan Kline <dylanekline@gmail.com>
2024-05-17 15:33:01 -0500

    Create, Update, Delete added to heat.py

--- weeklyhuntapi/views/heat.py
+++ weeklyhuntapi/views/heat.py
@@ -19,6 +19,24 @@ class HeatView(ViewSet):
         serializer = HeatSerializer(heats, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = HeatSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        heat = Heat.objects.get(pk=pk)
+        serializer = HeatSerializer(heat, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        heat = Heat.objects.get(pk=pk)
+        heat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class HeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Heat
