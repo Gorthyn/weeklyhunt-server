1139b2100f43f6041d594b45de1b78677812bdd8
Dylan Kline <dylanekline@gmail.com>
2024-05-23 11:14:17 -0500

    Create, Update, Delete added to moves.py

--- weeklyhuntapi/views/moves.py
+++ weeklyhuntapi/views/moves.py
@@ -19,6 +19,24 @@ class MoveView(ViewSet):
         serializer = MoveSerializer(moves, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = MoveSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        move = Move.objects.get(pk=pk)
+        serializer = MoveSerializer(move, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        move = Move.objects.get(pk=pk)
+        move.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class MoveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Move
