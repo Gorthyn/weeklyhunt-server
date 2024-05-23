341cb827afe3b146f94cb64b3a9a13fe00632395
Dylan Kline <dylanekline@gmail.com>
2024-05-23 11:12:47 -0500

    Create, Update, Delete added to mission.py

--- weeklyhuntapi/views/mission.py
+++ weeklyhuntapi/views/mission.py
@@ -19,6 +19,24 @@ class MissionView(ViewSet):
         serializer = MissionSerializer(missions, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = MissionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        mission = Mission.objects.get(pk=pk)
+        serializer = MissionSerializer(mission, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        mission = Mission.objects.get(pk=pk)
+        mission.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class MissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mission
