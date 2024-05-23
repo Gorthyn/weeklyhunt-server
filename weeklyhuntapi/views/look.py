6c9fe4e853212b1d68e283b88c51d17630f59913
Dylan Kline <dylanekline@gmail.com>
2024-05-23 10:59:44 -0500

    Create, Update, Delete added to look.py

--- weeklyhuntapi/views/look.py
+++ weeklyhuntapi/views/look.py
@@ -19,6 +19,24 @@ class LookView(ViewSet):
         serializer = LookSerializer(looks, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = LookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        look = Look.objects.get(pk=pk)
+        serializer = LookSerializer(look, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        look = Look.objects.get(pk=pk)
+        look.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class LookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Look
