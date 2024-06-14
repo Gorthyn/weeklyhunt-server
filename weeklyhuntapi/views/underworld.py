278e5ef524b8bdc13ed41feba05fa0baca090c42
Dylan Kline <dylanekline@gmail.com>
2024-06-14 14:01:59 -0500

    Create, Update, Delete added to underworld.py

--- weeklyhuntapi/views/underworld.py
+++ weeklyhuntapi/views/underworld.py
@@ -19,6 +19,24 @@ class UnderworldView(ViewSet):
         serializer = UnderworldSerializer(underworlds, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = UnderworldSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        underworld = Underworld.objects.get(pk=pk)
+        serializer = UnderworldSerializer(underworld, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        underworld = Underworld.objects.get(pk=pk)
+        underworld.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class UnderworldSerializer(serializers.ModelSerializer):
     class Meta:
         model = Underworld
