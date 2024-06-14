b0a79dee0382b0678b43d2309fa62c52ada73a55
Dylan Kline <dylanekline@gmail.com>
2024-06-14 13:58:48 -0500

    Create, Update, Delete added to reason.py

--- weeklyhuntapi/views/reason.py
+++ weeklyhuntapi/views/reason.py
@@ -19,6 +19,24 @@ class ReasonView(ViewSet):
         serializer = ReasonSerializer(reasons, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = ReasonSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        reason = Reason.objects.get(pk=pk)
+        serializer = ReasonSerializer(reason, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        reason = Reason.objects.get(pk=pk)
+        reason.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class ReasonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reason
