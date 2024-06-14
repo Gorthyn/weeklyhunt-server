001dbd32d36004813edcbae563640229e0ab97fd
Dylan Kline <dylanekline@gmail.com>
2024-06-14 14:00:24 -0500

    Create, Update, Delete added to resources.py

--- weeklyhuntapi/views/resources.py
+++ weeklyhuntapi/views/resources.py
@@ -19,6 +19,24 @@ class ResourcesView(ViewSet):
         serializer = ResourcesSerializer(resources, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = ResourcesSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        resource = Resources.objects.get(pk=pk)
+        serializer = ResourcesSerializer(resource, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        resource = Resources.objects.get(pk=pk)
+        resource.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class ResourcesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resources
