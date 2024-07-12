cd855945e0bf94e0a263b3f969411b4dbf751ef3
Dylan Kline <dylanekline@gmail.com>
2024-07-12 13:40:18 -0500

    Change to 2d6 dice roller

--- weeklyhuntapi/views/dice_roller.py
+++ weeklyhuntapi/views/dice_roller.py
@@ -3,16 +3,13 @@ from django.http import JsonResponse
 from weeklyhuntapi.models import DiceRoll
 
 def roll_2d6(request):
-    """
-    Roll two six-sided dice, apply an optional modifier, and return the results.
-    """
-    modifier = int(request.GET.get('modifier', 0))  # Get modifier from query params or default to 0
+    modifier = int(request.GET.get('modifier', 0))  # Optional modifier
     result_1 = random.randint(1, 6)
     result_2 = random.randint(1, 6)
     total = result_1 + result_2 + modifier
     
     # Save the result to the database
-    DiceRoll.objects.create(result_1=result_1, result_2=result_2, modifier=modifier, total=total)
+    DiceRoll.objects.create(result_1=result_1, result_2=result_2, modifier=modifier, total=total, roll_type='2d6')
     
     return JsonResponse({
         'result_1': result_1,
