586a97b22acfeefc6208d9d98115fccbb75bcefd
Dylan Kline <dylanekline@gmail.com>
2024-07-19 14:06:59 -0500

    Updated roll_2d6 function with caching

--- weeklyhuntapi/views/dice_roller.py
+++ weeklyhuntapi/views/dice_roller.py
@@ -1,10 +1,17 @@
 import random, logging
 from django.http import JsonResponse
+from django.core.cache import cache
 from weeklyhuntapi.models import DiceRoll
 
 logger = logging.getLogger(__name__)
 
 def roll_2d6(request):
+    cache_key = 'recent_2d6_rolls'
+    recent_rolls = cache.get(cache_key)
+
+    if recent_rolls:
+        return JsonResponse(recent_rolls)
+
     try:
         modifier = int(request.GET.get('modifier', 0))
         result_1 = random.randint(1, 6)
@@ -13,12 +20,17 @@ def roll_2d6(request):
         
         DiceRoll.objects.create(result_1=result_1, result_2=result_2, modifier=modifier, total=total, roll_type='2d6')
         
-        return JsonResponse({
+        response_data = {
             'result_1': result_1,
             'result_2': result_2,
             'modifier': modifier,
             'total': total
-        })
+        }
+        
+        # Cache the response for 1 hour
+        cache.set(cache_key, response_data, timeout=3600)
+
+        return JsonResponse(response_data)
     except Exception as e:
         logger.error(f"Failed to roll 2d6: {str(e)}")
         return JsonResponse({"error": "Error processing your dice roll"}, status=500)
